83cec411bd47c367aac9deb5e6836448b009d652
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-11-12 11:08:41 +0800

    create initial section seven

---
+++ src/pages/section-seven/index.js
@@ -0,0 +1,9 @@
+import { BannerHeader } from "../../components/header"
+
+export default function SectionSeven() {
+  return (
+    <section>
+      <BannerHeader title="贊助單位" />
+    </section>
+  )
+}
