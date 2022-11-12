b0b3fa0b71fe8c32086c3ebaae2cdbafd9635467
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-11-12 11:04:30 +0800

    change

--- src/pages/section-six/index.js
+++ src/pages/section-six/index.js
@@ -14,15 +14,13 @@ export default function SectionSix() {
         </div>
         <section className={style.section__inner}>
           <h3 className={style.title}>評審機制</h3>
-          <p className={style.content}>
+          <p>
             初選： 將由六角學院前端、UI 評審進行第一波篩選。 決選：
             由六角學院與贊助廠商討論，進行最後篩選，並於 12/30(五)
             由評審進行直播公布名單！
           </p>
-        </section>
-        <section className={style.section__inner}>
           <h3 className={style.title}>獎項</h3>
-          <ol className={`${style.list}`}>
+          <ol className={style.list}>
             <li>初選佳作 共六十位 數位獎狀</li>
             <li>
               個人企業獎 共六位 NTD <span>3,000</span> /位
