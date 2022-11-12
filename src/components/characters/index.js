c3df0ab305ac229d1bda7c6c3aa7b5ca80a4d245
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-11-12 22:49:44 +0800

    finish characters

--- src/components/characters/index.js
+++ src/components/characters/index.js
@@ -1,18 +1,18 @@
 import characterF2e from "../../assets/character/character_f2e.gif"
 import characterUi from "../../assets/character/character_ui.gif"
 import characterTeam from "../../assets/character/character_team.gif"
-import styles from "./style.module.scss"
+import style from "./style.module.scss"
 
 export default function Characters() {
   return (
-    <div className={styles.characters}>
-      <div>
+    <div className={style.characters}>
+      <div className={style.character}>
         <img src={characterF2e} alt="character f2e" />
       </div>
-      <div>
+      <div className={style.character_middle}>
         <img src={characterUi} alt="character ui" />
       </div>
-      <div>
+      <div className={style.character}>
         <img src={characterTeam} alt="character team" />
       </div>
     </div>
